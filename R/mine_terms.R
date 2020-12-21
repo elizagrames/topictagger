266156238e7ab35501bbc5ccb4b7a29f1494c8c7
Eliza Grames <eliza.grames@uconn.edu>
2020-12-21 14:24:02 -0500

    added annoying publisher terms to stopwords in mine_terms

--- R/mine_terms.R
+++ R/mine_terms.R
@@ -6,12 +6,30 @@
 #' @param ngrams logical: if TRUE, only returns phrases of two words or more
 #' @example inst/examples/mine_terms_ex.R
 #' @export
-mine_terms <- function(x, retain_stopwords=FALSE, known_phrases=NULL, ngrams=TRUE, new_stopwords=c()){
-  sw <- append(litsearchr::custom_stopwords, new_stopwords)
+mine_terms <- function(x, known_phrases=NULL, ngrams=TRUE, new_stopwords=c()){
+  publisher_terms <- c("study suggests", "results suggest", "multiple sites", "study area",
+                       "results indicate", "significant differences", "negative effect", "study aimed",
+                       "results demonstrate", "significantly lower", "findings suggest", "data suggest", "results indicated",
+                       "positively correlated", "positively related", "significantly reduced", "significantly improved",
+                       "study highlights", "significant difference", "previous studies", "plos one", "significantly  higher",
+                       "results show",  
+                       "rights reserved",
+                       "john wiley", "bmc genomics", "annual review", "annual reviews inc",
+                       "copyright holder", "copyright applies", "abstract may", "content may",
+                       "original published version", "s express written permission",
+                       "full abstract", "individual use", "email articles", 
+                       "springer nature", "public library", "elsevier b", "elsevier science", "mdpi publishing", 
+                       "biomed central", "international journal", "new march",
+                       "royal society", "national academy", "linnean society",
+                       "biological conservation", "plos neglected tropical diseases", 
+                       "academic press inc", "oxford university press", "pergamon press -",
+                       "abstracts", "download", "users may print", "listserv without", "present study")
+  new_stopwords <- append(new_stopwords, publisher_terms)
+  sw <- unique(append(stopwords::stopwords(), new_stopwords))
   
   # keep any stopwords supplied or in phrases
   # for example, if 'of' is actually important
-  if(retain_stopwords){
+  if(any(known_phrases)){
     retain <- sw %in% unlist(strsplit(known_phrases, " "))
     sw <- sw[!retain]
   }
@@ -25,6 +43,7 @@ mine_terms <- function(x, retain_stopwords=FALSE, known_phrases=NULL, ngrams=TRU
   z[which(z %in% sw)] <- "|"
   z <- unlist(strsplit(paste(z, collapse="_"), "\\|"))
   z <- trimws(gsub("_", " ", z))
+  z[which(z %in% sw)] <- "|"
   
   # remove junk
   if(any(z=="")){
