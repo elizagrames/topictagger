5576813e1ef6863ccc0c0d2a6317122ebc55fa53
Eliza Grames <eliza.grames@uconn.edu>
2020-12-21 14:25:31 -0500

    added function to fill blank rows in human-readable hierarchies

---
+++ R/fill_rows.R
@@ -0,0 +1,25 @@
+#' Fill in parents for nested entries
+#' @description Given a human-readable nested list, matches children and descendants to parent entries and fills in gaps.
+#' @param x a data.frame where columns represent, from left to right, nested levels of a hierarchical list
+#' @return a data.frame of the same dimensions as x with parent entries filled in
+#' @example inst/examples/fill_rows.R
+#' @export 
+fill_rows <- function(x){
+  if(any(x=="")){
+    x[x==""] <- NA
+  }
+    for(j in ncol(x):2){
+    for(i in nrow(x):1){
+      if(!is.na(x[i,j])){
+        if(is.na(x[i,(j-1)])){
+          parents <- x[1:i,(j-1)]
+          parent <- tail(parents[!is.na(parents)],1)
+          x[i,(j-1)] <- parent
+        }
+      }
+      
+    }
+    }
+  return(x)
+
+}
